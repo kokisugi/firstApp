187c74a935066628f72056b78f3e4ae880c0af26
Sugihara <s-koki.sugihara@rakuten.com>
2020-09-14 19:15:50 +0900

    tt

--- index02.php
+++ index02.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
   <body>
-<h1 id="title">Hello world!!!</h1>
+<h1 id="title">Hello worldxyz!!!</h1>
 <p class="description">CSSの入門！</p>
   </body>
 </html>
